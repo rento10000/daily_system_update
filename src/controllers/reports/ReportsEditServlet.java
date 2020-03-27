eebd632d43374a560e5f5a4cd7f5d339bd07e99e
nakoto <nakoto15021@yahoo.co.jp>
2020-03-27 20:25:21 +0900

    Add Report edit

--- src/controllers/reports/ReportsEditServlet.java
+++ src/controllers/reports/ReportsEditServlet.java
@@ -14,25 +14,27 @@ import models.Employee;
 import models.Report;
 import utils.DBUtil;
 
+
 @WebServlet("/reports/edit")
 public class ReportsEditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+
 
     public ReportsEditServlet() {
         super();
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
         Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
 
-        Employee login_employee = (Employee) request.getSession().getAttribute("login_employee");
-        if (r != null && login_employee.getId() == r.getEmployee().getId()) {
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
+        if(r != null && login_employee.getId() == r.getEmployee().getId()) {
             request.setAttribute("report", r);
             request.setAttribute("_token", request.getSession().getId());
             request.getSession().setAttribute("report_id", r.getId());
